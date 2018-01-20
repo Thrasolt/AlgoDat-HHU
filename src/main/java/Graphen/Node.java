31321fbad9c720037c7e6bad13a7f0e5b3047cc5
Thrasolt <timres@gmx.net>
2018-01-20 18:06:43 +0100

    gradle setup

--- src/main/java/Graphen/Node.java
+++ src/main/java/Graphen/Node.java
@@ -18,6 +18,22 @@ public class Node {
     this.key=key;
   }
 
+  public void addOutEdge(Edge e){
+    boolean edgeTest = e.node1 == this;
+    edgeTest = edgeTest && !this.outEdges.contains(e);
+    if(edgeTest) {
+      this.outEdges.add(e);
+    }
+  }
+
+  public void addInEdge(Edge e){
+    boolean edgeTest = e.node2 == this;
+    edgeTest = edgeTest && !this.inEdges.contains(e);
+    if(edgeTest) {
+      this.inEdges.add(e);
+    }
+  }
+
   // Methoden
   public int getKey(){
     return this.key;
