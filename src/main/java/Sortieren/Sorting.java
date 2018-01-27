e1f891e7380ded01e28795a50235042a4ccdb1f6
timtressel <timres@gmx.net>
2018-01-27 22:52:59 +0100

    more quicksort

--- src/main/java/Sortieren/Sorting.java
+++ src/main/java/Sortieren/Sorting.java
@@ -14,6 +14,58 @@ public class Sorting{
   }
 
   // Sortieralgorithmen
-  public static void quicksort() {
+  public static void quicksort(int[] series){
+    quicksort(series,0,series.length-1);
+  }
+  public static void quicksort(int[] series, int left, int right) {
+    System.out.println("Quicksort started");
+    int pivot = series[right];
+    int indexLeft = left;
+    int indexRight = right-1;
+    System.out.println("indexLeft start: "+indexLeft);
+
+    // Tauschprozess
+    while(indexLeft<indexRight){
+      System.out.println("Tauschprozess");
+      while(series[indexLeft] <= pivot && indexLeft<=right){
+        indexLeft += 1;
+        System.out.println("indexLeft: "+indexLeft);
+      }
+      while(series[indexRight] >= pivot && indexRight>=left){
+        indexRight -= 1;
+      }
+      if(indexLeft<indexRight){
+        System.out.println("Tausche: "+series[indexLeft]+" "+series[indexRight]);
+        swtch(series, indexLeft, indexRight);
+      }
+    }
+
+    // Pivot tauschen, falls laenge 2 und sinnvoll
+    if(right-left>1){
+      swtch(series, indexLeft, right);
+    } else if(series[indexLeft]>=pivot){
+      System.out.println("Tausche Pivot: "+series[indexLeft]+" "+pivot);
+      swtch(series, indexLeft, right);
+    }
+
+    /*
+    // Divide and Conquer
+    indexLeft -= 1; // move to position in front of pivot
+    if(indexLeft-left>1){
+      quicksort(series,left,indexLeft);
+    }
+    if(right-indexRight>1){
+      quicksort(series,indexRight,right);
+    }
+    */
+
+  }
+
+  public static String printSeries(int[] series){
+    String output = "";
+    for(int i = 0; i < series.length; i++){
+      output = output+Integer.toString(series[i])+" ";
+    }
+    return output.replaceAll("\\s+$", "");
   }
 }
--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -3,6 +3,40 @@ import static org.junit.Assert.*;
 
 public class SortingTest {
     @Test
-    public void testAppHasAGreeting() {
+    public void testQuickSort() {
+
+      Sorting sort = new Sorting();
+
+      int[] testSeries0 = {2,1};
+      sort.quicksort(testSeries0);
+      assertEquals("1 2",sort.printSeries(testSeries0));
+
+      int[] testSeries1 = {2,3};
+      sort.quicksort(testSeries1);
+      assertEquals("2 3",sort.printSeries(testSeries1));
+
+      int[] testSeries2 = {1,3,2};
+      sort.quicksort(testSeries2);
+      assertEquals("1 2 3",sort.printSeries(testSeries2));
+
+      /*
+
+      int[] testSeries2 = {2,1,5,4,3};
+      sort.quicksort(testSeries2);
+      assertEquals("1 2 3 4 5",sort.printSeries(testSeries2));
+
+
+      int[] testSeries10 = {3,4,7,1,2,11,12,10,9};
+      sort.quicksort(testSeries10);
+      assertEquals("1 2 3 4 7 9 10 11 12",sort.printSeries(testSeries10));
+
+      int[] testSeries20 = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+      sort.quicksort(testSeries20);
+      assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15",sort.printSeries(testSeries20));
+
+      int[] testSeries30 = {3,4,7,1,2,11,9,25};
+      sort.quicksort(testSeries30);
+      assertEquals("1 2 3 4 7 9 11 25",sort.printSeries(testSeries30));
+      */
     }
 }
