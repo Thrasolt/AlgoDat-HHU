654388688908802440e856d4eb9c2bbb10a9e18c
timtressel <timres@gmx.net>
2018-01-28 20:44:59 +0100

    quicksort works

--- src/main/java/Sortieren/Sorting.java
+++ src/main/java/Sortieren/Sorting.java
@@ -18,47 +18,29 @@ public class Sorting{
     quicksort(series,0,series.length-1);
   }
   public static void quicksort(int[] series, int left, int right) {
-    System.out.println("Quicksort started");
-    int pivot = series[right];
     int indexLeft = left;
-    int indexRight = right-1;
-    System.out.println("indexLeft start: "+indexLeft);
+    int indexRight = right;
+    int pivot = series[left + (right-left)/2];
 
-    // Tauschprozess
-    while(indexLeft<indexRight){
-      System.out.println("Tauschprozess");
-      while(series[indexLeft] <= pivot && indexLeft<=right){
-        indexLeft += 1;
-        System.out.println("indexLeft: "+indexLeft);
-      }
-      while(series[indexRight] >= pivot && indexRight>=left){
-        indexRight -= 1;
-      }
-      if(indexLeft<indexRight){
-        System.out.println("Tausche: "+series[indexLeft]+" "+series[indexRight]);
-        swtch(series, indexLeft, indexRight);
-      }
-    }
-
-    // Pivot tauschen, falls laenge 2 und sinnvoll
-    if(right-left>1){
-      swtch(series, indexLeft, right);
-    } else if(series[indexLeft]>=pivot){
-      System.out.println("Tausche Pivot: "+series[indexLeft]+" "+pivot);
-      swtch(series, indexLeft, right);
-    }
-
-    /*
-    // Divide and Conquer
-    indexLeft -= 1; // move to position in front of pivot
-    if(indexLeft-left>1){
-      quicksort(series,left,indexLeft);
-    }
-    if(right-indexRight>1){
-      quicksort(series,indexRight,right);
-    }
-    */
-
+    // Divide into two lists
+     while (indexLeft <= indexRight) {
+       while (series[indexLeft] < pivot) {
+           indexLeft++;
+       }
+       while (series[indexRight] > pivot) {
+           indexRight--;
+       }
+       if (indexLeft <= indexRight) {
+           swtch(series, indexLeft, indexRight);
+           indexLeft++;
+           indexRight--;
+       }
+     }
+     // divide and conquer
+     if (left < indexRight)
+         quicksort(series, left, indexRight);
+     if (indexLeft < right)
+         quicksort(series, indexLeft, right);
   }
 
   public static String printSeries(int[] series){
--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -15,16 +15,13 @@ public class SortingTest {
       sort.quicksort(testSeries1);
       assertEquals("2 3",sort.printSeries(testSeries1));
 
-      int[] testSeries2 = {1,3,2};
-      sort.quicksort(testSeries2);
-      assertEquals("1 2 3",sort.printSeries(testSeries2));
+      int[] testSeries3 = {1,3,2};
+      sort.quicksort(testSeries3);
+      assertEquals("1 2 3",sort.printSeries(testSeries3));
 
-      /*
-
-      int[] testSeries2 = {2,1,5,4,3};
-      sort.quicksort(testSeries2);
-      assertEquals("1 2 3 4 5",sort.printSeries(testSeries2));
-
+      int[] testSeries4 = {2,1,5,4,3};
+      sort.quicksort(testSeries4);
+      assertEquals("1 2 3 4 5",sort.printSeries(testSeries4));
 
       int[] testSeries10 = {3,4,7,1,2,11,12,10,9};
       sort.quicksort(testSeries10);
@@ -32,11 +29,10 @@ public class SortingTest {
 
       int[] testSeries20 = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
       sort.quicksort(testSeries20);
-      assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15",sort.printSeries(testSeries20));
+      assertEquals("0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15",sort.printSeries(testSeries20));
 
       int[] testSeries30 = {3,4,7,1,2,11,9,25};
       sort.quicksort(testSeries30);
       assertEquals("1 2 3 4 7 9 11 25",sort.printSeries(testSeries30));
-      */
     }
 }
