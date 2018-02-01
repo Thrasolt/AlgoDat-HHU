830874ab7f2868c8ff93d81dbba269ba07a674f2
Thrasolt <timres@gmx.net>
2018-02-01 22:10:00 +0100

    median of meds

--- src/main/java/Suchen/MedianOfMedians.java
+++ src/main/java/Suchen/MedianOfMedians.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MedianOfMedians{
 
   // Instanzvaribalen
@@ -5,12 +7,27 @@ public class MedianOfMedians{
 
   public static int findMedian(int[] folge, int i){
     int n = folge.length;
+    int nrPackes = n/groupSize;
+    int median;
 
     // Eingabepruefung
     if(n <= groupSize) {
-
+      return findMedianDirectly(folge);
     }
-    return 0;
+    else {
+      int[][] packs = createPackages(folge);
+      int[] medianList= new int[nrPackes];
+      for(int j=0; i<nrPackes; j++){
+        medianList[j] = findMedianDirectly(packs[j]);
+      }
+      if(medianList.length <= groupSize){
+        median = findMedianDirectly(medianList);
+      }
+      else{
+        median = findMedian(medianList, i);
+      }
+    }
+    return median;
   }
 
   public static int findMedianDirectly(int[] folge){
@@ -25,22 +42,67 @@ public class MedianOfMedians{
     return 0;
   }
 
-  public static int[][] splitSeries(int[] folge){
+  public static int[][] createPackages(int[] folge){
     int n = folge.length;
     int nrPackes = n/groupSize;
     if(n%groupSize!=0){
       nrPackes += 1;
     }
 
-    int[][] outArray = new  int[nrPackes][groupSize];
-
+    int[][] outArray = new int[nrPackes][groupSize];
+    int index = 0;
     for(int i=0; i<nrPackes; i++){
       for(int j=0; i<groupSize; j++){
-        
+        outArray[i][j]=folge[index];
+        index++;
       }
     }
 
     return outArray;
 
+    }
+
+    public static int[] partition(int[] series, int i, int medOfMeds) {
+      int left = 0;
+      int right =series.length-1;
+      int indexLeft = 0;
+      int indexRight = series.length-1;
+      int pivot = medOfMeds;
+
+      // Tauschprozess
+       while (indexLeft <= indexRight) {
+         while (series[indexLeft] < pivot) {
+             indexLeft++;
+         }
+         while (series[indexRight] > pivot) {
+             indexRight--;
+         }
+         if (indexLeft <= indexRight) {
+             swtch(series, indexLeft, indexRight);
+             indexLeft++;
+             indexRight--;
+         }
+       }
+
+      // divide and conquer
+      int[] leftSeries = Arrays.copyOfRange(series, left, indexRight);
+      if(leftSeries.length==i){
+        System.out.println(" = i");
+        return Arrays.copyOfRange(series, i+1, i+2);
+      }
+      if(leftSeries.length>i) {
+        System.out.println(" > i");
+        return leftSeries;
+      }
+      int[] rightSeries = Arrays.copyOfRange(series, indexLeft, right);
+      System.out.println(" < i");
+      return rightSeries;
+    }
+
+    // Hilfsmittel
+    public static void swtch(int[] folge, int i, int j){
+      int temp = folge[i];
+      folge[i] = folge[j];
+      folge[j] = temp;
     }
 }
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -6,6 +6,11 @@ public class SearchTest {
     public void testMedianOfMedians() {
         MedianOfMedians medofMed = new MedianOfMedians();
 
+        int[] testPart = {1,2,3};
+        int[] medPart = medofMed.partition(testPart, 2, 2);
+        assertEquals(1,medPart.length);
+        assertEquals(2,medPart[2]);
+
         int[] testUtilfindeMed= {2,1,5,4,3};
         assertEquals(3,medofMed.findMedianDirectly(testUtilfindeMed));
     }
